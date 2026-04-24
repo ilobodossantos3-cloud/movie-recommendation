f26501f74b7bdaeac81ca39dd0417d857dd8d1b9
Igor Santos <ilobodossantos3@gmail.com>
2026-04-24 01:13:38 +0100

    Delete veil-browser directory

--- veil-browser/src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-// Prevents the default action for some events
-window.addEventListener('contextmenu', (e) => {
-  // In production, you might want to implement a custom context menu
-  // e.preventDefault()
-})
